975dfebeae0b8166d123e333a8af9c9c73d0c85c
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-01-21 16:55:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoquestEditor::Application.config.secret_token = '78a4576d7a3ab203d49ad8b6de55c6108baeaee0e7a1b9301345d35d147813a659c26640a3f02e777030df546c796a7cbfd6b2543c2a8f92f8f4052fb435274a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GeoquestEditor::Application.config.session_store :cookie_store, :key => '_Geoquest Editor_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# GeoquestEditor::Application.config.session_store :active_record_store
