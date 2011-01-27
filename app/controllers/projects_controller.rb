adc4287bc7e3f9a472621cc4f9b3bca66d108415
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-01-27 16:07:10 +0100

    User kann Projekte auswählen. refs 7

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,14 @@ class ProjectsController < ApplicationController
     @projects = Project.find_all_by_user_id(@current_user.id)
   end
 
+  def show
+    @project = Project.find(:first, :conditions => {:id => params[:id], :user_id => @current_user.id})
+
+    if @project.nil?
+      redirect_to projects_path, :notice => "A project with this id does not exist or it is not yours"
+    end
+  end
+
   def new
     @project = Project.new
     @project.user = @current_user
