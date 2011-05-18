8649ab326936af78623a50f549bdd698e77e37c6
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-05-18 23:01:59 +0200

    Subordner beim Bilder hochladen. Closes #12

--- app/controllers/image_gallery_controller.rb
+++ app/controllers/image_gallery_controller.rb
@@ -2,16 +2,30 @@ class ImageGalleryController < ApplicationController
 
   before_filter :authenticate
 
+
+  class Helper
+    include Singleton
+    include ImageGalleryHelper 
+  end
+
   def show
       mission_query = 'doc("game.xml")/game/mission'
       adapter = ExistAdapter.new(params[:project_id])
       @project = Project.find(:first, :conditions => {:id => params[:project_id], :user_id => @current_user.id})
       @missions = adapter.do_request(mission_query);
+      @directories = Helper.instance.list_image_directories(@project.id)
   end
 
   def uploadFile
       name = params[:uploadedFile].original_filename
-      directory = Rails.root.join("public/projects", params[:project_id], "drawable")
+      directory = Rails.root.join("public/projects", params[:project_id], "drawable").to_s
+      directory += params[:folder].to_s
+      Rails.logger.warn(directory)
+      
+      # Directory should end with a "/"
+      if directory[directory.length() -1 ] != "/" then
+        directory += "/"
+      end
 
       UploadedFile.save(name, directory, params[:uploadedFile])
       redirect_to :action => 'show'
--- app/helpers/image_gallery_helper.rb
+++ app/helpers/image_gallery_helper.rb
@@ -1,2 +1,28 @@
+
 module ImageGalleryHelper
+  def list_image_directories(id)
+
+    path =Rails.root.join("public", "projects", id.to_s, "drawable")
+
+    # DFS search
+    stack= ["/"]
+    dirs = []
+
+    while stack.size() > 0 do
+      dir = stack.pop()
+      if dir == "/" then
+        dirs += [dir]
+      else
+        dirs += [dir.chop()] #remove last "/"
+      end
+      Dir.chdir(path.to_s + dir)
+      Dir.glob("*") {|x|
+        if File.directory?(x) then
+            stack.push(dir + File.basename(x).to_s + "/")
+        end
+      }
+    end
+
+    dirs
+  end
 end
