08687e96e7bff7fa4f7f68cb9a8a2e1221125e82
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-06-21 12:28:46 +0200

    Funktionelle Navigation mittels jstree

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,19 +1,20 @@
 require 'queries/query'
 require 'queries/file_queries'
+require 'exist_adapter'
 #require 'cgi'
 
 class QueryController < ApplicationController
 
- def execute
+  def execute
     query = nil
 
     case params[:query]
-      when "DirectoryQuery"
-        query = DirectoryQuery.new(params)
-      else
-        warning = "Unsupported Query: "
-        warning += params[:query] unless params[:query].nil?
-        logger.warn  warning
+    when "DirectoryQuery"
+      query = DirectoryQuery.new(params)
+    else
+      warning = "Unsupported Query: "
+      warning += params[:query] unless params[:query].nil?
+      logger.warn  warning
     end
 
     unless query.nil? then
@@ -25,15 +26,15 @@ class QueryController < ApplicationController
 
   end
 
- # Based on the:
- #
- # jQuery File Tree Ruby Connector
- #
- # Version 1.01
- #
- # Erik Lax
- # http://datahack.se
- # 13 July 2008
+  # Based on the:
+  #
+  # jQuery File Tree Ruby Connector
+  #
+  # Version 1.01
+  #
+  # Erik Lax
+  # http://datahack.se
+  # 13 July 2008
   def show_dir
     #TODO: 37 => :project_id
     root = Rails.root.join("public", "projects", params[:project_id].to_s).to_s
@@ -49,20 +50,20 @@ class QueryController < ApplicationController
 
     Rails.logger.warn("Still ok")
     
-      path = root + "/" + dir
-      Rails.logger.warn("Path: " + path.to_s)
+    path = root + "/" + dir
+    Rails.logger.warn("Path: " + path.to_s)
 
-      # chdir() to user requested dir (root + "/" + dir)
-      Dir.chdir(File.expand_path(path).untaint);
-      # check that our base path still begins with root path
-      if Dir.pwd[0,root.length] == root then
-        Rails.logger.warn("Still in root")
-        #loop through all directories
-        Dir.glob("*") {
-          |x|
-          if not File.directory?(x.untaint) then next end
-          text += "<li class=\"directory collapsed\"><a href=\"#\" rel=\"#{dir}#{x}/\">#{x}</a></li>";
-        }
+    # chdir() to user requested dir (root + "/" + dir)
+    Dir.chdir(File.expand_path(path).untaint);
+    # check that our base path still begins with root path
+    if Dir.pwd[0,root.length] == root then
+      Rails.logger.warn("Still in root")
+      #loop through all directories
+      Dir.glob("*") {
+        |x|
+        if not File.directory?(x.untaint) then next end
+        text += "<li class=\"directory collapsed\"><a href=\"#\" rel=\"#{dir}#{x}/\">#{x}</a></li>";
+      }
 
       #loop through all files
       Dir.glob("*") {
@@ -76,50 +77,76 @@ class QueryController < ApplicationController
       #only happens when someone tries to go outside your root directory...
       text += "You are way out of your league"
     end
-  text += "</ul>"
-  render :content_type => "text/html", :text => text
+    text += "</ul>"
+    render :content_type => "text/html", :text => text
   end
 
+  def get_mission_as_hash(mission)
+    mission_data = {
+      "data" => {
+        "title" => mission.attributes['id'],
+        "icon" => "mission",
+        "attr" => {
+          "href" => project_mission_path(params[:project_id], mission.attributes['id'])
+        }
+      },
+      "metadata" => {"mission_id" => mission.attributes['id']},
+    }
+    if(REXML::XPath.match(mission, './mission').length > 0) # Mission has submissions
+      mission_data["children"] = []
+      mission_data["state"] = "open"
+      REXML::XPath.match(mission, './mission').each do |submission|
+        mission_data["children"] += [ get_mission_as_hash(submission) ]
+      end
+    end
+    return mission_data
+  end
 
   def show_missions
-    json_data = <<-EOF
-    {
-	"data" : {
-		"title" : "Game",
-		"attr" : {},
-		"icon" : "game"
- 	},
 
-	"metadata" : {"mission_id" : -1},
-	"language" : "en",
-  "state" : "open",
-  "children" : [
-   {
-	"data" : {
-		"title" : "Introtext",
-		"icon" : "npc-icon"
- 	},
+    json_data = ""
+    adapter = ExistAdapter.new(params["project_id"]);
 
-	"metadata" : {"mission_id" : 0},
-	"language" : "en"
- },
+    if params["mission_id"] == "-1" then
 
-   {
-	"data" : {
-		"title" : "Mapoverview",
-		"icon" : "map-icon"
- 	},
+      # All top level missions:
+      template = ERB.new 'doc("game.xml")/game/mission'
+      query = template.result(binding)
+      Rails.logger.info(query)
 
-	"metadata" : {"mission_id" : 1},
-	"language" : "en",
-"state" : "closed"
- }
+      results = adapter.do_request(query);
 
 
-]
- }
+      game = {
+        "data" => {
+          "title" => "Game",
+          "icon" => "game"
+        },
+        "metadata" => {"mission_id" => -1},
+        "state" => "open",
+        "children" => []
+      }
 
-EOF
+      results.each do |mission|
+        mission_data = get_mission_as_hash(mission)
+        game['children'] += [mission_data];
+        Rails.logger.info(mission.attributes['id'])
+      end
+
+
+      json_data = ActiveSupport::JSON.encode(game)
+
+
+    else
+      template = ERB.new 'doc("game.xml")//mission[@id = "<%= params["mission_id"] %>"]'
+      query = template.result(binding)
+      results = adapter.do_request(query);
+
+      mission_data = get_mission_as_hash(results[0])
+      json_data = ActiveSupport::JSON.encode(mission_data)
+    end
+
+    
     render :content_type => "application/json", :text => json_data
   end
 
