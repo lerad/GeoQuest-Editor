f8224f2ff624959c9786c9ef49eddb3e5f6a1c91
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-06-24 09:40:38 +0200

    Unnötigen Code aus dem MapOverview-View gelöscht

--- public/javascripts/views/map_overview.js
+++ public/javascripts/views/map_overview.js
@@ -1,43 +1,21 @@
 
 var map;
-var hotspotCreating = false;
-var hotspotDeleting = false;
 var markerList = new Array();
 
 // This is needed to convert a Point to a LatLng...
 var overlay = new google.maps.OverlayView();
 overlay.draw = function() {};
 
+var hotspotRadiusDialogHtml = 'Radius (m): <input id="hotspotRadius" type="text" size="4" /><br /> \
+                               <input type="button" id="changeRadiusButton" value="Ok" />'
 
-function onMapClick(event) {
+var hotspotRadiusDialog = null;
 
-    if (hotspotCreating) {
-        var id = prompt("Marker id");
-
-        if(markerList[id] != null) {
-            alert("Id is already given to another hotspot");
-            return;
-        }
-
-        var lng = event.latLng.lng();
-        var lat = event.latLng.lat();
-
-        hotspotCreating = false;
-
-        var cmd = new AddHotspotCommand();
-        cmd.setParameter("latitude", lat);
-        cmd.setParameter("longitude", lng);
-        cmd.setParameter("id", id);
-        cmd.setParameter("mission_id", mission_id);
-        cmd.setParameter("project_id", project_id);
-        cmd.execute();
-    }
-}
+var imageSelector = new ImageSelector(null);
 
 
-// this is called when the page loads.
-// it initializes the map, and creates each marker
-function initialize() {
+function initializeMap() {
+    // Address of the university building at the "Römerstraße" in Bonn
     var latlng = new google.maps.LatLng(50.718437, 7.118864);
     var myOptions = {
         zoom: 15,
@@ -47,47 +25,23 @@ function initialize() {
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-    google.maps.event.addListener(map, "click", onMapClick);
     overlay.setMap(map);
-
-    addHotspotMarker();
-
 }
 
-function createNewHotspot() {
-    hotspotCreating = true;
-    hotspotDeleting = false; // If it is still active
-}
+    
 
-function deleteHotspot() {
-    hotspotCreating = false;
-    hotspotDeleting = true; 
-}
+function initializeRadiusDialog() {
 
 
-var hotspotRadiusDialogHtml = 'Radius (m): <input id="hotspotRadius" type="text" size="4" /><br /> \
-                               <input type="button" id="changeRadiusButton" value="Ok" />'
+    hotspotRadiusDialog = $('<div></div>')
+    .html(hotspotRadiusDialogHtml)
+    .dialog({
+        autoOpen: false,
+        title: 'Radius',
+        width: 300
 
-var selectedMarker = null;
-var selectedImage = "";
-var hotspotRadiusDialog = $('<div></div>')
-.html(hotspotRadiusDialogHtml)
-.dialog({
-    autoOpen: false,
-    title: 'Radius',
-    width: 300
+    });
 
-});
-
-var imageSelector = new ImageSelector(null);
-
-
-function changeHotspotImage(file) {
-    $("#hotspotImage").attr("src", "/projects/" + project_id + "/" + file);
-    selectedImage = file;
-}
-
-$(document).ready(function() {
     $("#changeRadiusButton").click(function() {
         hotspotRadiusDialog.dialog('close');
         var cmd = new UpdateHotspotCommand();
@@ -99,26 +53,70 @@ $(document).ready(function() {
         cmd.execute();
     });
 
+}
 
+
+
+function initializeMapContextMenu() {
+    $("#map_canvas").contextMenu({
+        menu: 'mapMenu'
+    },
+    function(action, el, pos) {
+        if (action == "newHotspot") createHotspot(pos);
+    });
+}
+
+
+$(document).ready(function() {
+    initializeMap();
+    addHotspotMarkerToMap();
+    initializeRadiusDialog();
+    initializeMapContextMenu();
 });
 
+/**
+ * Adds a contextmenu to a marker
+ * The markers div element is found via the path of the markers image
+ * E.g. The image tag with the image /images/marker1.png
+ * As these are not necessarily unique, an ID should be added to the path
+ * E.g. /images/marker1.png#hotspot1
+ */
+function initializeMarkerContextMenu(marker, imagePath) {
+        $("img[src=\"" + imagePath + "\"]").parent().contextMenu(
+        {
+            menu: 'hotspotMenu'
+        },
+        function(action, el, pos) {
+            if (action == "deleteHotspot") {
+                var cmd = new DeleteHotspotCommand();
+                cmd.setParameter("id", marker.hotspot_id);
+                cmd.setParameter("project_id", project_id);
+                cmd.setParameter("mission_id", mission_id);
+                cmd.execute();
+            }
+            else if (action == "changeImage") {
+                // Remove existing initiation, so only one is always open
+                // Otherwise it might lead to problems with the ids.
+                imageSelector.setCallback(function(file) {
+                    marker.customIcon = file;
+                    var cmd = new UpdateHotspotCommand();
+                    cmd.setParameter("project_id", project_id);
+                    cmd.setParameter("mission_id", mission_id);
+                    cmd.setParameter("hotspot_id", marker.hotspot_id);
+                    cmd.setParameter("radius", marker.circle.radius);
+                    cmd.setParameter("image", file);
+                    cmd.execute();
+                });
+                imageSelector.show();
+            }
+            else if (action == "changeRadius") {
+                $("#hotspotRadius").attr("value", marker.radius);
+                hotspotRadiusDialog.marker = marker;
+                hotspotRadiusDialog.dialog("open");
+            }
+        });
 
 
-function openHotspotDialog(marker) {
-    selectedMarker = marker;
-    selectedImage = marker.customIcon;
-    
-    if(marker.customIcon == "") {
-        $("#hotspotImage").attr("src","/images/empty32.gif");
-    }
-    else {
-        $("#hotspotImage").attr("src","/projects/" + project_id + "/" +marker.customIcon);
-    }
-
-    $("#hotspotRadius").attr("value", marker.circle.getRadius());
-
-
-    dialog.dialog("open");
 }
 
 function addMarker(lat, lng, hotspot_id, _radius, image) {
@@ -129,6 +127,9 @@ function addMarker(lat, lng, hotspot_id, _radius, image) {
     }
 
 
+    /**
+     * Add marker to map
+     */
     var myLatlng = new google.maps.LatLng(lat,lng);
 
     var marker = new google.maps.Marker({
@@ -139,23 +140,34 @@ function addMarker(lat, lng, hotspot_id, _radius, image) {
     });
 
 
+    /**
+     * Save some variables
+     */
 
     marker.customIcon = image;
     marker.hotspot_id = hotspot_id;
     marker.radius = _radius;
+    markerList[hotspot_id] = marker;
 
+    /**
+     * Set the icon
+     * The postfix "#<id>" is added to find the element later
+     */
     var path = "";
 
-    if(image == "") { // Use default icon:
+    if(image == "") { 
+        // Use default icon. This is done explicitely, so the id can be included
         path = "http://www.google.com/mapfiles/marker.png#" + hotspot_id;
     }
     else {
-        path = "/projects/" + project_id + "/" + image + "#" + hotspot_id; // + "#<id>" to find the element later
+        path = "/projects/" + project_id + "/" + image + "#" + hotspot_id; 
     }
 
     marker.setIcon(path);
 
-    // Add a 30m Radius to the marker.
+    /**
+     * Add a circle to the marker
+     */
     var circle = new google.maps.Circle({
         map: map,
         radius: _radius
@@ -167,64 +179,14 @@ function addMarker(lat, lng, hotspot_id, _radius, image) {
 
 
     // It seems as if the image is not added directly. Thus a little time
-    // must pass until the contextmenu gets added
-    setTimeout(function() {
-        $("img[src=\"" + path + "\"]").parent().contextMenu(
-            {menu: 'hotspotMenu'},
-            function(action, el, pos) {
-                if (action == "deleteHotspot") {
-                    var cmd = new DeleteHotspotCommand();
-                    cmd.setParameter("id", hotspot_id);
-                    cmd.setParameter("project_id", project_id);
-                    cmd.setParameter("mission_id", mission_id);
-                    cmd.execute();
-                }
-                else if (action == "changeImage") {
-                    // Remove existing initiation, so only one is always open
-                    // Otherwise it might lead to problems with the ids.
-                    imageSelector.setCallback(function(file) {
-                        marker.customIcon = file;
-                         var cmd = new UpdateHotspotCommand();
-                         cmd.setParameter("project_id", project_id);
-                         cmd.setParameter("mission_id", mission_id);
-                         cmd.setParameter("hotspot_id", hotspot_id);
-                         cmd.setParameter("radius", marker.circle.radius);
-                         cmd.setParameter("image", file);
-                         cmd.execute();
-                    });
-                    imageSelector.show();
-                }
-              else if (action == "changeRadius") {
-                  $("#hotspotRadius").attr("value", marker.radius);
-                  hotspotRadiusDialog.marker = marker;
-                  hotspotRadiusDialog.dialog("open");
-              }
-            });
-
-        }, 1000);
+    // must pass until the contextmenu gets added, otherwise the element is not
+    // found
+    setTimeout(function() { initializeMarkerContextMenu(marker, path); }, 1000);
     
 
-
-
-
-    markerList[hotspot_id] = marker;
-
-
-    google.maps.event.addListener(marker, 'click', function() {
-        if(hotspotDeleting) {
-            hotspotDeleting = false;
-            var cmd = new DeleteHotspotCommand();
-            cmd.setParameter("id", marker.getTitle());
-            cmd.setParameter("project_id", project_id);
-            cmd.setParameter("mission_id", mission_id);
-            cmd.execute();
-        }
-        else {
-
-            openHotspotDialog(marker);
-        }
-    });
-
+    /**
+     * Add events
+     */
     google.maps.event.addListener(marker, "dragend", function() {
         var cmd = new MoveHotspotCommand();
         cmd.setParameter("id", marker.getTitle());
@@ -241,7 +203,7 @@ function getMarker(id) {
     return markerList[id];
 }
 
-function createHotspotFromContextMenu(pos) {
+function createHotspot(pos) {
     point = new google.maps.Point(pos.x, pos.y);
     latlng = overlay.getProjection().fromContainerPixelToLatLng(point);
 
@@ -257,17 +219,4 @@ function createHotspotFromContextMenu(pos) {
 
 }
 
-$(document).ready(function() {
-
-    $("#map_canvas").contextMenu({
-        menu: 'mapMenu'
-    },
-    function(action, el, pos) {
-        if (action == "newHotspot") createHotspotFromContextMenu(pos);
-    });
-
-});
-
-
-$(document).ready(initialize);
 
