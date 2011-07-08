8627dd0b307d22bfb5c0948c0a1a5721be01dba8
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-07-08 16:57:19 +0200

    Etwas den mission_logic Code aufgeräumt

--- public/javascripts/views/mission_logic.js
+++ public/javascripts/views/mission_logic.js
@@ -1,3 +1,9 @@
+/*****************************************************
+ *  Event Dialog
+ *  Used to create new Events
+ *****************************************************/
+
+
 // Initialize Event Dialog:
 $(document).ready(function() {
     $("#eventDialog_dialog").dialog({
@@ -64,6 +70,29 @@ $(document).ready(function() {
 
 });
 
+// Initialisation function of the dialog.
+// It is called everytime the dialog is openened
+function initEventDialog(type) {
+    $("#eventDialog_dialog").data("geoquest.event_type", type);
+    $("#eventDialog_dialog").data("geoquest.requirements", []);
+    $("#eventDialog_dialog").data("geoquest.commands", []);
+    $("#eventDialog_nextMission").selectOptions("none", true);
+    $(".event-dialog-dynamic-content").remove();
+}
+
+// Adds a new requirement to the dialog
+function addRequirementToEventDialog(requirement) {
+     $("#eventDialog_dialog").data("geoquest.requirements").push(requirement);
+     newElement = $("<li></li>").text(requirement.description)
+                                .addClass("event-dialog-dynamic-content")
+     $("#eventDialog_addRequirementListEntry").before(newElement);
+}
+
+/*****************************************************
+ * Mission Requirement Dialog
+ * Used to create new requirements of the type "reqMissionStatus"
+ *****************************************************/
+
 // Initialize Mission Requirement Dialog
 $(document).ready(function() {
     $("#missionRequirementDialog_dialog").dialog({
@@ -84,142 +113,8 @@ $(document).ready(function() {
 
 });
 
-function recomputeReqInHotspotRangeStatus() {
-    var hotspot = $("#inHotspotRangeRequirementDialog_hotspot").val();
-    var hotspot_name = $("#inHotspotRangeRequirementDialog_hotspot option:selected").text();
-
-    var requirement_text = "If the player is inside of the range of " + hotspot_name;
-
-    xml = '<reqInRange id="' + hotspot + '" />';
-
-    requirement = {
-        "type" : "reqInRange",
-        "requiredHotspot" : hotspot,
-        "requiredHotspotName" : hotspot_name,
-        "description" : requirement_text,
-        "xml" : xml
-    };
-
-    $("#inHotspotRangeRequirementDialog_dialog").data("geoquest.requirement", requirement);
-
-    $("#inHotspotRangeRequirementDialog_requirement").text(requirement_text);
-
-}
-
-
-function recomputeReqOutHotspotRangeStatus() {
-    var hotspot = $("#outHotspotRangeRequirementDialog_hotspot").val();
-    var hotspot_name = $("#outHotspotRangeRequirementDialog_hotspot option:selected").text();
-
-    var requirement_text = "If the player is outside of the range of " + hotspot_name;
-
-    xml = '<reqOutRange id="' + hotspot + '" />';
-
-    requirement = {
-        "type" : "reqOutRange",
-        "requiredHotspot" : hotspot,
-        "requiredHotspotName" : hotspot_name,
-        "description" : requirement_text,
-        "xml" : xml
-    }
-
-    $("#outHotspotRangeRequirementDialog_dialog").data("geoquest.requirement", requirement);
-
-    $("#outHotspotRangeRequirementDialog_requirement").text(requirement_text);
-
-}
-
-// Initialize In Hotspot Range Requirement Dialog
-$(document).ready(function() {
-    $("#inHotspotRangeRequirementDialog_dialog").dialog({
-        autoOpen: false,
-        title: "Create new reqInRange requirement",
-        width: 400,
-        height: 300
-    });
-
-    $("#inHotspotRangeRequirementDialog_hotspot").change(recomputeReqInHotspotRangeStatus)
-    $("#inHotspotRangeRequirementDialog_createButton").click(function() {
-       requirement = $("#inHotspotRangeRequirementDialog_dialog").data("geoquest.requirement");
-       $("#inHotspotRangeRequirementDialog_dialog").dialog("close");
-       addRequirementToEventDialog(requirement);
-    });
-
-});
-
-// Initialize Out Hotspot Range Requirement Dialog
-$(document).ready(function()  {
-    $("#outHotspotRangeRequirementDialog_dialog").dialog({
-        autoOpen: false,
-        title: "Create new reqOutRange requirement",
-        width: 400,
-        height: 300
-    });
-
-    $("#outHotspotRangeRequirementDialog_hotspot").change(recomputeReqOutHotspotRangeStatus)
-    $("#outHotspotRangeRequirementDialog_createButton").click(function() {
-       requirement = $("#outHotspotRangeRequirementDialog_dialog").data("geoquest.requirement");
-       $("#outHotspotRangeRequirementDialog_dialog").dialog("close");
-       addRequirementToEventDialog(requirement);
-    });
-
-});
-
-
-function recomputeReqAttributeStatus() {
-    name =     $("#attributeRequirementDialog_attribute").val();
-    value = $("#attributeRequirementDialog_value").val();
-
-    requirement_text = 'If the attribute "' + name + '" has the value "' + value + '"';
-    xml = '<reqAttribute name="' + name + '" value="' + value + '" />';
-
-
-    requirement = {
-        "type" : "reqAttribute",
-        "attribute" : name,
-        "value" : value,
-        "description" : requirement_text,
-        "xml" : xml
-    }
-
-    $("#attributeRequirementDialog_dialog").data("geoquest.requirement", requirement);
-
-    $("#attributeRequirementDialog_requirement").text(requirement_text);
-
-}
-
-
-// Initialize Attribute Requirement Dialog
-$(document).ready(function() {
-    $("#attributeRequirementDialog_dialog").dialog({
-        autoOpen: false,
-        title: "Create new reqAttribute requirement",
-        width: 400,
-        height: 300
-    });
-
-    $("#attributeRequirementDialog_attribute").change(recomputeReqAttributeStatus)
-    $("#attributeRequirementDialog_value").change(recomputeReqAttributeStatus)
-    $("#attributeRequirementDialog_attribute").keyup(recomputeReqAttributeStatus)
-    $("#attributeRequirementDialog_value").keyup(recomputeReqAttributeStatus)
-
-    $("#attributeRequirementDialog_createButton").click(function() {
-       requirement = $("#attributeRequirementDialog_dialog").data("geoquest.requirement");
-       $("#attributeRequirementDialog_dialog").dialog("close");
-       addRequirementToEventDialog(requirement);
-    });
-
-});
-
-
-function initEventDialog(type) {
-    $("#eventDialog_dialog").data("geoquest.event_type", type);
-    $("#eventDialog_dialog").data("geoquest.requirements", []);
-    $("#eventDialog_dialog").data("geoquest.commands", []);
-    $("#eventDialog_nextMission").selectOptions("none", true);
-    $(".event-dialog-dynamic-content").remove();
-}
-
+// Initialisation function of the dialog.
+// It is called everytime the dialog is openened
 function initReqMissionStatusDialog() {
     $(".missionRequirementDialog-checkbox").attr("checked", false);
     // None does not exist in this list, but in this way the list is set back
@@ -228,35 +123,8 @@ function initReqMissionStatusDialog() {
     recomputeReqMissionStatus();
 }
 
-
-
-function initReqInHotspotRangeDialog() {
-    $("#inHotspotRangeRequirementDialog_hotspot option:first-child").attr("selected", "selected");
-    recomputeReqInHotspotRangeStatus();
-}
-
-function initReqOutHotspotRangeDialog() {
-    $("#outHotspotRangeRequirementDialog_hotspot option:first-child").attr("selected", "selected");
-    recomputeReqOutHotspotRangeStatus();
-}
-
-function initReqAttributeDialog() {
-    $("#attributeRequirementDialog_attribute").val("");
-    $("#attributeRequirementDialog_value").val("");
-    recomputeReqAttributeStatus();
-}
-
-
-// Adds a new requirement to the dialog
-function addRequirementToEventDialog(requirement) {
-     $("#eventDialog_dialog").data("geoquest.requirements").push(requirement);
-     newElement = $("<li></li>").text(requirement.description)
-                                .addClass("event-dialog-dynamic-content")
-     $("#eventDialog_addRequirementListEntry").before(newElement);
-}
-
-
-// Recomputes the mission Status requirement, if it is changed in the corresponding dialog
+// Is called everytime a value changes and
+// recomputes the requirement object, and its description
 function recomputeReqMissionStatus() {
     var _new = $("#missionRequirementDialog_new").is(':checked');
     var fail = $("#missionRequirementDialog_fail").is(':checked');
@@ -315,6 +183,189 @@ function recomputeReqMissionStatus() {
 
 
 }
+
+/*****************************************************
+ * In Hospot Range Requirement Dialog
+ * Used to create new requirements of the type "reqInRange"
+ *****************************************************/
+
+
+
+
+// Initialize In Hotspot Range Requirement Dialog
+$(document).ready(function() {
+    $("#inHotspotRangeRequirementDialog_dialog").dialog({
+        autoOpen: false,
+        title: "Create new reqInRange requirement",
+        width: 400,
+        height: 300
+    });
+
+    $("#inHotspotRangeRequirementDialog_hotspot").change(recomputeReqInHotspotRange)
+    $("#inHotspotRangeRequirementDialog_createButton").click(function() {
+       requirement = $("#inHotspotRangeRequirementDialog_dialog").data("geoquest.requirement");
+       $("#inHotspotRangeRequirementDialog_dialog").dialog("close");
+       addRequirementToEventDialog(requirement);
+    });
+
+});
+
+
+// Is called everytime a value changes and
+// recomputes the requirement object, and its description
+function recomputeReqInHotspotRange() {
+    var hotspot = $("#inHotspotRangeRequirementDialog_hotspot").val();
+    var hotspot_name = $("#inHotspotRangeRequirementDialog_hotspot option:selected").text();
+
+    var requirement_text = "If the player is inside of the range of " + hotspot_name;
+
+    xml = '<reqInRange id="' + hotspot + '" />';
+
+    requirement = {
+        "type" : "reqInRange",
+        "requiredHotspot" : hotspot,
+        "requiredHotspotName" : hotspot_name,
+        "description" : requirement_text,
+        "xml" : xml
+    };
+
+    $("#inHotspotRangeRequirementDialog_dialog").data("geoquest.requirement", requirement);
+
+    $("#inHotspotRangeRequirementDialog_requirement").text(requirement_text);
+}
+
+// Initialisation function of the dialog.
+// It is called everytime the dialog is openened
+function initReqInHotspotRangeDialog() {
+    $("#inHotspotRangeRequirementDialog_hotspot option:first-child").attr("selected", "selected");
+    recomputeReqInHotspotRange();
+}
+
+/*****************************************************
+ * Out Hospot Range Requirement Dialog
+ * Used to create new requirements of the type "reqOutRange"
+ *****************************************************/
+
+
+
+// Initialize Out Hotspot Range Requirement Dialog
+$(document).ready(function()  {
+    $("#outHotspotRangeRequirementDialog_dialog").dialog({
+        autoOpen: false,
+        title: "Create new reqOutRange requirement",
+        width: 400,
+        height: 300
+    });
+
+    $("#outHotspotRangeRequirementDialog_hotspot").change(recomputeReqOutHotspotRange)
+    $("#outHotspotRangeRequirementDialog_createButton").click(function() {
+       requirement = $("#outHotspotRangeRequirementDialog_dialog").data("geoquest.requirement");
+       $("#outHotspotRangeRequirementDialog_dialog").dialog("close");
+       addRequirementToEventDialog(requirement);
+    });
+
+});
+
+// Initialisation function of the dialog.
+// It is called everytime the dialog is openened
+function initReqOutHotspotRangeDialog() {
+    $("#outHotspotRangeRequirementDialog_hotspot option:first-child").attr("selected", "selected");
+    recomputeReqOutHotspotRange();
+}
+
+// Is called everytime a value changes and
+// recomputes the requirement object, and its description
+function recomputeReqOutHotspotRange() {
+    var hotspot = $("#outHotspotRangeRequirementDialog_hotspot").val();
+    var hotspot_name = $("#outHotspotRangeRequirementDialog_hotspot option:selected").text();
+
+    var requirement_text = "If the player is outside of the range of " + hotspot_name;
+
+    xml = '<reqOutRange id="' + hotspot + '" />';
+
+    requirement = {
+        "type" : "reqOutRange",
+        "requiredHotspot" : hotspot,
+        "requiredHotspotName" : hotspot_name,
+        "description" : requirement_text,
+        "xml" : xml
+    }
+
+    $("#outHotspotRangeRequirementDialog_dialog").data("geoquest.requirement", requirement);
+
+    $("#outHotspotRangeRequirementDialog_requirement").text(requirement_text);
+
+}
+
+
+/*****************************************************
+ * Attribute Requirement Dialog
+ * Used to create new requirements of the type "reqAttribute"
+ *****************************************************/
+
+
+
+// Initialize Attribute Requirement Dialog
+$(document).ready(function() {
+    $("#attributeRequirementDialog_dialog").dialog({
+        autoOpen: false,
+        title: "Create new reqAttribute requirement",
+        width: 400,
+        height: 300
+    });
+
+    $("#attributeRequirementDialog_attribute").change(recomputeReqAttribute);
+    $("#attributeRequirementDialog_value").change(recomputeReqAttribute);
+    $("#attributeRequirementDialog_attribute").keyup(recomputeReqAttribute);
+    $("#attributeRequirementDialog_value").keyup(recomputeReqAttribute);
+
+    $("#attributeRequirementDialog_createButton").click(function() {
+       requirement = $("#attributeRequirementDialog_dialog").data("geoquest.requirement");
+       $("#attributeRequirementDialog_dialog").dialog("close");
+       addRequirementToEventDialog(requirement);
+    });
+
+});
+
+// Initialisation function of the dialog.
+// It is called everytime the dialog is openened
+function initReqAttributeDialog() {
+    $("#attributeRequirementDialog_attribute").val("");
+    $("#attributeRequirementDialog_value").val("");
+    recomputeReqAttribute();
+}
+
+// Is called everytime a value changes and
+// recomputes the requirement object, and its description
+function recomputeReqAttribute() {
+    name =     $("#attributeRequirementDialog_attribute").val();
+    value = $("#attributeRequirementDialog_value").val();
+
+    requirement_text = 'If the attribute "' + name + '" has the value "' + value + '"';
+    xml = '<reqAttribute name="' + name + '" value="' + value + '" />';
+
+
+    requirement = {
+        "type" : "reqAttribute",
+        "attribute" : name,
+        "value" : value,
+        "description" : requirement_text,
+        "xml" : xml
+    }
+
+    $("#attributeRequirementDialog_dialog").data("geoquest.requirement", requirement);
+
+    $("#attributeRequirementDialog_requirement").text(requirement_text);
+
+}
+
+
+
+
+/*********************************
+ * Other
+ * (Ajax, Jsplump, Contextmenu)
+ *********************************/
 
 // from can be either a mission or a hotspot description. It only has to have an id
 function addJsplumbConnection( from, event) {
