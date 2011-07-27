f6abce77f6d7f4574f75cde13fbae92da96e60da
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-07-27 02:02:04 +0200

    QRTagReading Mission wird vollständig unterstüzt

--- public/javascripts/views/qr_tag_reading.js
+++ public/javascripts/views/qr_tag_reading.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
    $("#editTaskDescription").editable(onTaskDescriptionChange, { placeholder: "Click to add a text (optional)" });
    $("#editEndButtonText").editable(onEndButtonChange, { placeholder: "Click to add a text (optional)" });
+   $("#editFeedBackText").editable(onFeedBackTextChange, { placeholder: "Click to add a text (optional)" });
+   $("#editExpectedContent").editable(onExpectedContentChange, { placeholder: "Click to specify the expected content" });
+   $("#editIfRight").editable(onIfRightChange, { placeholder: "Click to add a text (optional)" });
+   $("#editIfWrong").editable(onIfWrongChange, { placeholder: "Click to add a text (optional)" });
+
+   $("#treasureRadio").change(onModeChange);
+   $("#productRadio").change(onModeChange);
 
 });
+
+function onModeChange() {
+    var mode = $("input[@name='mode']:checked").val();
+
+    if(mode == "treasure") {
+        $("#treasureAttributes").removeClass("hidden");
+        $("#productAttributes").addClass("hidden");
+    }
+
+    if(mode == "product") {
+        $("#treasureAttributes").addClass("hidden");
+        $("#productAttributes").removeClass("hidden");
+    }
+
+    cmd = new UpdateAttributeInMissionCommand();
+    cmd.setParameter("project_id", project_id);
+    cmd.setParameter("mission_id", mission_id);
+    cmd.setParameter("attribute", "mode");
+    cmd.setParameter("value", mode);
+    cmd.execute();
+}
 
 var imageSelector = new ImageSelector(null);
 
@@ -42,3 +70,45 @@ function onEndButtonChange(value, settings) {
     cmd.execute();
     return value;
 }
+
+function onFeedBackTextChange(value, settings) {
+    cmd = new UpdateAttributeInMissionCommand();
+    cmd.setParameter("project_id", project_id);
+    cmd.setParameter("mission_id", mission_id);
+    cmd.setParameter("attribute", "feedbacktext");
+    cmd.setParameter("value", value);
+    cmd.execute();
+    return value;
+}
+
+function onExpectedContentChange(value, settings) {
+    cmd = new UpdateAttributeInMissionCommand();
+    cmd.setParameter("project_id", project_id);
+    cmd.setParameter("mission_id", mission_id);
+    cmd.setParameter("attribute", "expected_content");
+    cmd.setParameter("value", value);
+    cmd.execute();
+    return value;
+}
+
+function onIfRightChange(value, settings) {
+    cmd = new UpdateAttributeInMissionCommand();
+    cmd.setParameter("project_id", project_id);
+    cmd.setParameter("mission_id", mission_id);
+    cmd.setParameter("attribute", "if_right");
+    cmd.setParameter("value", value);
+    cmd.execute();
+    return value;
+}
+
+function onIfWrongChange(value, settings) {
+    cmd = new UpdateAttributeInMissionCommand();
+    cmd.setParameter("project_id", project_id);
+    cmd.setParameter("mission_id", mission_id);
+    cmd.setParameter("attribute", "if_wrong");
+    cmd.setParameter("value", value);
+    cmd.execute();
+    return value;
+}
+
+
