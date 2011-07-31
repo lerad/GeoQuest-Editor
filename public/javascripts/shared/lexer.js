17f2c941242d9ad3546be26f2f29947b2fc2e206
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-07-31 15:36:08 +0200

    Parser + Lexer hinzugefügt

---
+++ public/javascripts/shared/lexer.js
@@ -0,0 +1,146 @@
+String.prototype.startsWith = function(str) {
+    return (this.match("^" + str) == str)
+}
+
+function Lexer() {
+
+
+    this.getToken = function(string) {
+        this.string = string;
+        this.position = 0;
+        token = [];
+        next = this.getNextToken()
+        while(next != null) {
+            token.push(next);
+            next = this.getNextToken();
+        }
+        return token;
+    }
+
+    this.getNextToken = function() {
+
+        while(this.peekChar() == ' ') this.readChar(); // Skip whitespaces
+
+        currentChar = this.readChar();
+        switch(currentChar) {
+            case null: 
+                return null
+            case '(':
+                return this.createToken(TokenType.BracketOpen, "(");
+            case ')':
+                return this.createToken(TokenType.BracketClose, ")");
+            case ',':
+                return this.createToken(TokenType.Comma, ",");
+            case '=':
+                return this.createToken(TokenType.Equal, "=");
+            case '<':
+                if(this.peekChar() == '=') {
+                    this.readChar();
+                    return this.createToken(TokenType.LesserEqual, "<=");
+                }
+                else 
+                    return this.createToken(TokenType.Lesser, "<");
+            case '>':
+                if(this.peekChar() == '=') {
+                    this.readChar();
+                    return this.createToken(TokenType.GreaterEqual, ">=");
+                }
+                else 
+                    return this.createToken(TokenType.Greater, ">");
+            case '0': case '1': case '2':
+            case '3': case '4': case '5':
+            case '6': case '7': case '8': case '9':
+                return this.readNumber( new String(currentChar) );
+            default:
+                return this.readString( new String(currentChar) )
+                
+        
+        }
+    }
+    
+    this.peekChar = function() {
+        if(this.position  >= this.string.length) 
+            return null;
+        else
+            return this.string[this.position];
+    }
+
+    this.readChar = function() {
+        if(this.position >= this.string.length) 
+            return null;
+        else
+            myChar = this.string[this.position];
+            this.position++;
+            return myChar; 
+    }
+
+    this.createToken = function(type, string) {
+        return {
+            "type" : type,
+            "string" : string
+        }
+    }
+
+    this.readNumber = function(currentString) {
+        numeric = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+        while (numeric.indexOf( this.peekChar() ) != -1) {
+            currentString += this.readChar();
+        }
+        if(this.peekChar() != '.') {
+            return this.createToken(TokenType._Number, currentString);
+        }
+        currentString += this.readChar(); // Read the '.'
+        while (numeric.indexOf( this.peekChar() ) != -1) {
+            currentString += this.readChar();
+        }
+        return this.createToken(TokenType._Number, currentString);
+    }
+
+
+    this.readString = function(currentString) {
+        validStartChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+        validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789";
+        if(validStartChars.indexOf(currentString[0]) == -1) {
+            alert("String may not start with '" + currentString[0] + "'");
+        }        
+        
+        while(validChars.indexOf(this.peekChar()) != -1) {
+            currentString += this.readChar();
+        }
+
+        lowerCaseString = currentString.toLowerCase();
+
+        if(lowerCaseString == "and") return this.createToken(TokenType.And, currentString);
+        if(lowerCaseString == "or") return this.createToken(TokenType.Or, currentString);
+        if(lowerCaseString == "isfailed") return this.createToken(TokenType.IsFailed, currentString);
+        if(lowerCaseString == "issuccess") return this.createToken(TokenType.IsSuccess, currentString);
+        if(lowerCaseString == "isnew") return this.createToken(TokenType.IsNew, currentString);
+        if(lowerCaseString == "isrunning") return this.createToken(TokenType.IsRunning, currentString);
+        if(lowerCaseString == "true") return this.createToken(TokenType._True, currentString);
+        if(lowerCaseString == "not") return this.createToken(TokenType.Not, currentString);
+
+        return this.createToken(TokenType.Variable, currentString);
+    }
+
+}
+TokenType = {
+    BracketOpen : 0,
+    BracketClose : 1,
+    And : 2,
+    Or : 3,
+    IsFailed : 5,
+    IsSuccess : 6,
+    IsNew : 7,
+    IsRunning : 8,
+    Variable : 9,
+    Greater : 10,
+    Lesser : 11,
+    GreaterEqual : 12,
+    LesserEqual : 13,
+    Equal : 14,
+    _Number : 15,
+    Comma : 16,
+    _True : 17,
+    Not : 18
+
+}
---
+++ public/javascripts/shared/parser.js
@@ -0,0 +1,286 @@
+function Parser() {
+
+    this.parse = function(tokenArray) {
+        // Recursive remove all brackets:
+
+        var parserArray = this.toParserArray(tokenArray);
+        
+        parserArray = this.parseStatus(parserArray);
+        
+        condition  = this.parseCondition(parserArray);
+         
+        return this.toAjaxTree(condition);
+    }
+
+    this.parseStatus = function(parserArray) {
+        position = 0;
+        while(position < parserArray.length) {
+            states = [TokenType.IsFailed, TokenType.IsSuccess, TokenType.IsRunning, TokenType.IsNew ];
+            if( states.indexOf( parserArray[position].token.type ) != -1) {
+                hasStatusPosition = position;
+                parserArray[hasStatusPosition].data = {
+                    "mission" : null
+                };
+                position++;
+                if (parserArray[position].token.type != TokenType.BracketOpen) {
+                    throw "Expected '(' after Status Query";
+                }
+                position++;
+                if (parserArray[position].token.type != TokenType.Variable) {
+                    throw "Expected mission ID as first parameter for HasStatus";
+                }
+                parserArray[hasStatusPosition].data["mission"] = parserArray[position].token["string"];
+                position++;
+                if (parserArray[position].token.type != TokenType.BracketClose) {
+                    throw "Expected ')' after Status Query";
+                }
+
+           
+                len = position - hasStatusPosition;
+                parserArray.splice(hasStatusPosition +1, len);
+                position = hasStatusPosition + 1;
+
+            }
+            else
+                position++;
+            
+        }
+        return parserArray;
+    }
+
+    this.parseCondition = function( parserArray) {
+
+        if(parserArray.length == 1) return parserArray[0];
+
+        // Check for brackets:
+        var i = 0;
+        while( i < parserArray.length ) {
+            if(parserArray[i].token.type == TokenType.BracketOpen) {
+                // Find corresponding close Bracket:
+                var hierarchy = 0;
+                var startBracket = i;
+                var pos = i+1;
+                
+                if(pos >=  parserArray.length) {
+                    throw "Cannot find corresponding ')' bracket";
+                }
+                while (!((hierarchy == 0) && (parserArray[pos].token.type == TokenType.BracketClose))) {
+                    
+                    if(parserArray[pos].token.type == TokenType.BracketOpen) hierarchy++;
+                    if(parserArray[pos].token.type == TokenType.BracketClose) hierarchy--;
+
+                    pos++;
+                    if(pos >= parserArray.length) {
+                        throw "Cannot find corresponding ')' bracket to to  " + parserArray[startBracket].token["string"] + parserArray[startBracket+1].token["string"]  ;
+                    }
+
+
+                }
+                var endBracket = pos;
+                var subArray = $.extend(true, [], parserArray.slice(startBracket + 1, endBracket)); // Deep copy
+                var myParseArray = parserArray.toSource();
+                var subCondition = this.parseCondition( subArray );
+                parserArray.splice(startBracket+1, endBracket - startBracket);
+                parserArray[ startBracket ] = subCondition;
+                i = startBracket;
+            }
+            i++;
+        }
+
+        if(parserArray.length == 1) return parserArray[0]; //Might happen after the brackets are evaluated
+
+
+        // Check for NOT's
+
+        i=0; 
+        while (i < parserArray.length) {
+            if (parserArray[i].token.type == TokenType.Not) {
+                if(parserArray[i].children.length > 0) { // Already parsed
+                    i++;
+                    continue;
+                }
+                next = i+1;
+                if(next >= parserArray.length) throw "Not shall not be the last element";
+                parserArray[i].children = [ parserArray[next] ];
+                parserArray.splice(next, 1);
+            }
+            i++;
+        }
+
+
+
+
+        if(parserArray.length == 1) return parserArray[0]; //Might happen after the brackets are evaluated
+
+        // Check for AND's:
+        i = 0;
+        while (i < parserArray.length ) {
+            if(parserArray[i].token.type == TokenType.And) { 
+                var conditions = []
+                if (i == 0) throw "AND should not be the first entry";
+                var start = i-1;
+                conditions.push(parserArray[start]);
+                while( true ) {
+                    i++;
+                    if( i >= parserArray.length) throw "AND should not be the last entry";
+                    conditions.push(parserArray[i]);
+                    i++;
+                    if (i >= parserArray.length) break;
+                    if (parserArray[i].token.type != TokenType.And) break;
+                }
+                var end = i;
+                parserArray[start] = {
+                    token : parserArray[start + 1].token,
+                    children : conditions
+                };
+                parserArray.splice(start+1, end-start);
+                i = start + 1;
+            }
+            i++;
+        }
+
+        if(parserArray.length == 1) return parserArray[0]; //Might happen after the brackets are evaluated
+
+        // Check for OR's:
+        i = 0;
+        while (i < parserArray.length ) {
+            if(parserArray[i].token.type == TokenType.Or) { 
+                var conditions = []
+                if (i == 0) throw "OR should not be the first entry";
+                var start = i-1;
+                conditions.push(parserArray[start]);
+                while( true ) {
+                    i++;
+                    if( i >= parserArray.length) throw "OR should not be the last entry";
+                    conditions.push(parserArray[i]);
+                    i++;
+                    if (i >= parserArray.length) break;
+                    if (parserArray[i].token.type != TokenType.Or) break;
+                }
+                var end = i;
+                parserArray[start] = {
+                    token : parserArray[start + 1].token,
+                    children : conditions
+                };
+                parserArray.splice(start+1, end-start);
+                i = start + 1;
+            }
+            i++;
+        }
+
+
+        if (parserArray.length == 1) return parserArray[0];
+
+        // Check for number comparisions:
+        numberComparisions = [TokenType.Lesser, TokenType.Greater, TokenType.Equal, TokenType.LesserEqual, TokenType.GreaterEqual];
+        i=0;
+        while( i < parserArray.length) {
+            if( numberComparisions.indexOf( parserArray[i].token.type ) != -1) {
+                comparisionType = parserArray[i].token.type;
+                if ( i == 0) throw "Comparisionelement should not be the first entry";
+                var start = i -1;
+                var elements = [];
+                elements.push(parserArray[ start ]);
+                while(true) {
+                    i++;
+                    if (i >= parserArray.length) {
+                        throw "Comparisionelement should not be the last entry";
+                    }
+                    elements.push(parserArray[i]);
+                    i++;
+                    if (i >= parserArray.length) break;
+                    if (parserArray[i].token.type != comparisionType) break;
+                }
+
+                var end = i;
+                parserArray[start] = {
+                    token : parserArray[start + 1].token,
+                    children : elements
+                };
+            parserArray.splice(start+1, end-start);
+            i = start + 1;
+            }
+          i++;
+        }
+
+        if (parserArray.length == 1) return parserArray[0];
+        else {
+           throw "Expected parserArray with length 1";
+        }
+    }
+
+
+    this.toParserArray = function(tokenArray) {
+        var parserArray = [];
+        for ( token in tokenArray) {
+            parserObject = {
+                "token" : tokenArray[token],
+                "children" : [],
+                "data" : {}
+            }
+            parserArray.push(parserObject);
+        
+        }
+        return parserArray;
+    
+    }
+
+    // Remove all the parsing stuff
+    this.toAjaxTree = function(condition) {
+
+        // This happens if toAjaxTree is called twice for a condition
+        // This should not happen, but apparently it does?
+        if(!condition.token.type) {
+            return condition;
+        }
+        for(i = 0; i< condition.children.length; i++) {
+            condition.children[i] = this.toAjaxTree(condition.children[i]);
+        }
+
+
+
+        if (condition.token.type == TokenType.IsFailed) {
+            condition.data.state = "fail";
+        }
+        if (condition.token.type == TokenType.IsSuccess) {
+            condition.data.state = "success";
+        }
+        if (condition.token.type == TokenType.IsNew) {
+            condition.data.state = "new";
+        }
+        if (condition.token.type == TokenType.IsRunning) {
+            condition.data.state = "running";
+        }
+        if (condition.token.type == TokenType.Variable) {
+            condition.data.name = condition.token["string"];
+        }
+        if (condition.token.type == TokenType._Number) {
+            condition.data.value = condition.token["string"];
+        }
+       
+        condition.token = TokenName[condition.token.type];        
+
+
+
+        return condition;
+    }
+
+}
+
+TokenName = [];
+TokenName[ TokenType.And ] =  "and";
+TokenName[ TokenType.Or ] = "or";
+TokenName[ TokenType.IsFailed ] = "missionState";
+TokenName[ TokenType.IsSuccess ] =  "missionState";
+TokenName[ TokenType.IsNew ] =  "missionState";
+TokenName[ TokenType.IsRunning ] = "missionState";
+TokenName[ TokenType.Variable ] =  "var";
+TokenName[ TokenType.Greater ] =  "gt";
+TokenName[ TokenType.Lesser ] = "lt";
+TokenName[ TokenType.GreaterEqual ] = "geq";
+TokenName[ TokenType.LesserEqual ] = "leq";
+TokenName[ TokenType.Equal ] = "eq";
+TokenName[ TokenType._Number ] = "num";
+TokenName[ TokenType._True ] = "true";
+TokenName[ TokenType.Not ] =  "not";
+
