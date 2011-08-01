6c8cc1e00e419cd715fe5513a631fdd8b13c3422
Folker Hoffmann <hoffman1@cs.uni-bonn.de>
2011-08-01 23:03:14 +0200

    Rückmeldung beim Speichern der XML-Datei

--- public/javascripts/commands/xmlfile.js
+++ public/javascripts/commands/xmlfile.js
@@ -5,6 +5,18 @@ function UpdateXmlFileCommand() {
     this.updateGui = function()  {
         // Do nothing. Probably later save it somewhere for a reset button.
     }
+
+    this.onSuccess = function() {
+        $("#messageField").removeClass("failure")
+                          .addClass("success")
+                          .text("xml file saved");
+    }
+
+    this.onFailure = function() {
+        $("#messageField").removeClass("success")
+                          .addClass("failure")
+                          .text("error during saving");
+    }
 }
 
 UpdateXmlFileCommand.prototype = new Command();
